25014ff8aab586992416ace593251e13b59188c4
theostavrides <theostavrides@gmail.com>
2019-05-27 20:43:52 -0700

    add board component to game

---
+++ reactserver/src/Board.js
@@ -0,0 +1,45 @@
+const React = require('react')
+const Chess = require('react-chess')
+const ChessJS = require('chess.js');
+const game = new ChessJS();
+
+require('./board.css')
+
+class Board extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    //initial position should be passed in as props
+    this.state = {pieces: []};
+    this.handleMovePiece = this.handleMovePiece.bind(this);
+  }
+
+  handleMovePiece(piece, fromSquare, toSquare) {
+
+    console.log(game.move({ from: fromSquare, to: toSquare }))
+
+    const newPieces = this.state.pieces
+      .map((curr, index) => {
+        if (piece.index === index) {
+          return `${piece.name}@${toSquare}`
+        } else if (curr.indexOf(toSquare) === 2) {
+          return false // To be removed from the board
+        }
+        return curr
+      })
+      .filter(Boolean)
+
+    this.setState({pieces: newPieces})
+  }
+
+  render() {
+    const {pieces} = this.state
+    return (
+      <div className="board">
+        <Chess pieces={pieces} onMovePiece={this.handleMovePiece} />
+      </div>
+    )
+  }
+}
+
+module.exports = Board
---
+++ reactserver/src/Room.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Board from './Board';
+
+class Room extends Component {
+  render() {
+    return (
+      <Board />
+    );
+  }
+}
+
+export default Room;
