ebeca7d55d4a6215e1f877eb5e2174fe436888e2
theostavrides <theostavrides@gmail.com>
2019-06-04 12:48:41 -0700

    clean up

--- reactserver/src/components/room/Video.js
+++ reactserver/src/components/room/Video.js
@@ -5,19 +5,7 @@ function Video() {
 
 
   return (
-    <body onload="my_init()">
-
-      <div id="otherClients">
-      </div>
-
-      <video  style={{float:"left"}} id="self" width="300" height="200">
-      </video>
-
-      <div style={{position:"relative", float:"left", width:"300px"}}>
-        <video id="caller" width="300" height="200"></video>
-      </div>
-
-  </body>
+    <div></div>
   )
 }
 
