41d3c5abcac37118de46595e251e7a5fd0c28c82
theostavrides <theostavrides@gmail.com>
2019-06-05 00:52:17 -0700

    WORKS!!!!!

--- reactserver/public/js/videologic.js
+++ reactserver/public/js/videologic.js
@@ -2,7 +2,7 @@ var selfEasyrtcid = "";
 
 
 function connect() {
-    easyrtc.setSocketUrl("192.168.88.85:8080")
+    easyrtc.setSocketUrl("https://www.theostavrides.com")
     easyrtc.setVideoDims(400,300);
     easyrtc.setRoomOccupantListener(convertListToButtons);
     easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
